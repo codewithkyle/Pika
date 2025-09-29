66930565d02b0f657ee47a0c2a9a2bb202b83a48
Kyle Andrews <codingwithkyle@gmail.com>
2025-09-29 19:47:21 -0400

    Cleaning up some unused utility functions I copied from another project

--- main.js
+++ main.js
@@ -115,22 +115,6 @@ function cstrlen(mem, ptr) {
     return len;
 }
 
-function color_hex_unpacked(r, g, b, a) {
-    r = r.toString(16).padStart(2, "0");
-    g = g.toString(16).padStart(2, "0");
-    b = b.toString(16).padStart(2, "0");
-    a = a.toString(16).padStart(2, "0");
-    return `#${r}${g}${b}${a}`;
-}
-
-function color_hex(color) {
-    const r = ((color>>(0*8))&0xFF).toString(16).padStart(2, "0");
-    const g = ((color>>(1*8))&0xFF).toString(16).padStart(2, "0");
-    const b = ((color>>(2*8))&0xFF).toString(16).padStart(2, "0");
-    const a = ((color>>(3*8))&0xFF).toString(16).padStart(2, "0");
-    return `#${r}${g}${b}${a}`;
-}
-
 const debounce = (callback, wait) => {
   let timeoutId = null;
 
